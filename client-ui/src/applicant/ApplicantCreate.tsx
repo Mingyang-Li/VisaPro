fa42f5e49c3adc710c095424cc5895b15aa15290
Mingyang Li <mingyangli118@gmail.com>
2022-04-03 10:48:38 +1200

    Reconfigured linting with cli

--- client-ui/src/applicant/ApplicantCreate.tsx
+++ client-ui/src/applicant/ApplicantCreate.tsx
@@ -1,25 +1,9 @@
 import * as React from "react";
 
-import {
-  Create,
-  SimpleForm,
-  CreateProps,
-  BooleanInput,
-  DateTimeInput,
-  ReferenceInput,
-  SelectInput,
-  ReferenceArrayInput,
-  SelectArrayInput,
-} from "react-admin";
+import { Create, SimpleForm, CreateProps, BooleanInput } from "react-admin";
 
-import { UserTitle } from "../user/UserTitle";
-import { EducationHistoryTitle } from "../educationHistory/EducationHistoryTitle";
-import { EmploymentHistoryTitle } from "../employmentHistory/EmploymentHistoryTitle";
-import { FamilyMemberTitle } from "../familyMember/FamilyMemberTitle";
-import { PersonalInfoTitle } from "../personalInfo/PersonalInfoTitle";
 import { PersonalInfoCreate } from "../personalInfo/PersonalInfoCreate";
 import { EducationHistoryCreate } from "../educationHistory/EducationHistoryCreate";
-import { TravelHistoryTitle } from "../travelHistory/TravelHistoryTitle";
 import { EmploymentHistoryCreate } from "../employmentHistory/EmploymentHistoryCreate";
 import { TravelHistoryCreate } from "../travelHistory/TravelHistoryCreate";
 import { FamilyMemberCreate } from "../familyMember/FamilyMemberCreate";
