aa8956f9f783fd06e475805371122f92f720f982
Mingyang Li <mingyangli118@gmail.com>
2022-05-02 21:28:51 +1200

    Added tabs for edit modal

--- client-ui/src/templates/applicantFormEdit/ApplicantFormEdit.tsx
+++ client-ui/src/templates/applicantFormEdit/ApplicantFormEdit.tsx
@@ -3,6 +3,84 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Button } from '@mui/material';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export function BasicTabs() {
+  const [value, setValue] = React.useState(4);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Personal Info" {...a11yProps(0)} />
+          <Tab label="Education History" {...a11yProps(1)} />
+          <Tab label="Employment History" {...a11yProps(2)} />
+          <Tab label="Travel History" {...a11yProps(3)} />
+          <Tab label="Family Members" {...a11yProps(4)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}
 
 interface IApplicantFormEdit {
   id?: string;
@@ -29,14 +107,17 @@ const ApplicantFormEdit: React.FC = (props: IApplicantFormEdit) => {
         Edit
       </Button>
       <Dialog
+        maxWidth={'lg'}
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth
       >
-        <DialogTitle>Edit</DialogTitle>
-        <br></br>
-        <DialogContent></DialogContent>
+        <DialogTitle>Edit applicant</DialogTitle>
+        <DialogContent>
+          <BasicTabs />
+        </DialogContent>
       </Dialog>
     </>
   );
