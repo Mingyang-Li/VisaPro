80308f7839548caef8403241aec437bd3486ceba
Mingyang Li <mingyangli118@gmail.com>
2022-07-05 11:59:00 +1200

    feat: added spinner

---
+++ client-ui/src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const loadingImg =
+  'https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg';
+
+interface ILoadingSpinner {
+  show: boolean;
+  text?: string;
+}
+
+const LoadingSpinner: React.FC<ILoadingSpinner> = (props: ILoadingSpinner) => (
+  <div
+    className="spinner"
+    style={{
+      display: props.show ? 'flex' : 'none',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      verticalAlign: 'baseline',
+      height: '100%',
+    }}
+  >
+    <img
+      src={loadingImg}
+      alt="Loading..."
+      style={{ top: '30%', left: '45%' }}
+    />
+    <h3>{props.text ?? 'Loading...'}</h3>
+  </div>
+);
+
+export default LoadingSpinner;
