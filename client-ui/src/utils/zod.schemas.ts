ade87822a2892855270b0725632dd24eb9c695d6
Mingyang Li <mingyangli118@gmail.com>
2022-07-24 00:26:27 +1200

    fix: renamed PersonalInfoSchema

--- client-ui/src/utils/zod.schemas.ts
+++ client-ui/src/utils/zod.schemas.ts
@@ -5,7 +5,7 @@ export const loginSchema = object({
   password: string().min(1, { message: 'Password is required' }),
 });
 
-export const SPersonalInfo = object({
+export const PersonalInfoSchema = object({
   firstName: string().min(1, { message: 'First name is required' }),
   lastName: string().min(1, { message: 'Last name is required' }),
   email: string().email().min(1, { message: 'Email is required' }),
