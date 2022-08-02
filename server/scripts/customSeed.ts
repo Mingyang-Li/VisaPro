fdd7c0109f394bf10aa16fa9804986ae2ab76db5
Mingyang Li <mingyangli118@gmail.com>
2022-08-02 20:00:22 +1200

    db: seeding script for applicants

--- server/scripts/customSeed.ts
+++ server/scripts/customSeed.ts
@@ -1,6 +1,4 @@
-import { user } from './../../client-ui/src/graphql/Store';
-import { Salt } from './../src/auth/password.service';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 import { hash } from "bcrypt";
 import { parseSalt } from "../src/auth/password.service";
 import { CreateUserArgs } from '../src/user/base/CreateUserArgs';
@@ -11,13 +9,12 @@ export async function customSeed() {
 
   //replace this sample code to populate your database
   //with data that is required for your application to start
-  await seedUsers();
+  await seedApplicants();
 
   client.$disconnect();
 }
 
 const USER_CT = 5;
-const APPLICANT_CT = 5;
 const ENTITY_CT = 5;
 
 // DB seeding plan for dev work on a new machine
@@ -30,7 +27,7 @@ const ENTITY_CT = 5;
 //   - Create 5 TravelHistory
 //   - Create 5 FamilyMember
 
-const seedUsers = async () => {
+export const seedUsers = async () => {
   const { BCRYPT_SALT } = process.env;
   const salt = parseSalt(BCRYPT_SALT);
   for (let i = 0; i < USER_CT; i++) {
@@ -53,4 +50,29 @@ const seedUsers = async () => {
     }
   }
   console.log(`❎ ${USER_CT} Users seeded to DB`);
+}
+
+export const seedApplicants = async () => {
+  const users = await client.user.findMany({});
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    for (let k = 0; k < ENTITY_CT; k++) {
+      const applicant: Prisma.ApplicantCreateArgs = {
+        data: {
+          archived: false,
+          createdBy: {
+            connect: {
+              id: user.id,
+            }
+          }
+        }
+      }
+      try {
+        await client.applicant.create(applicant);
+      } catch (error) {
+        throw error;
+      }
+      console.log(`❎ ${ENTITY_CT} Applicants for ${user.username} seeded to DB`);
+    }
+  }
 }
