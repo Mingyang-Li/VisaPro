366e332764f2bd31f1e4a3fa0bc3b38b21d21dd9
Mingyang-Li <mingyangli118@gmail.com>
2022-08-27 16:21:25 +1200

    feat: init base for curd on work-history

---
+++ client-ui/src/templates/employmentHistoryCreate/EmploymentHistoryCreate.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EmploymentHistoryCreate = () => <h2>EmploymentHistoryCreate</h2>;
+
+export default EmploymentHistoryCreate;
---
+++ client-ui/src/templates/employmentHistoryTable/EmploymentHistoryTable.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EmploymentHistoryTable = () => <h1>EmploymentHistoryTable</h1>;
+
+export default EmploymentHistoryTable;
